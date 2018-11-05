cae1eefa45a1ff324a608f60d5f60be54d3b9e05
Thomas Rasmussen <thomas.rasmussen@bestseller.com>
2018-11-05 03:22:17 +0100

    All gui done except easterEgg

--- src/yatzy/MainAppGUI.java
+++ src/yatzy/MainAppGUI.java
@@ -6,12 +6,14 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
+import javafx.scene.input.MouseButton;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 import javax.management.StringValueExp;
+import java.util.Optional;
 
 public class MainAppGUI extends Application {
     public static void main(String[] args) {
@@ -34,8 +36,8 @@ public class MainAppGUI extends Application {
 
     // -------------------------------------------------------------------------
 
-    // The Yatzy game object
-    private Yatzy yatzy = new Yatzy();
+    // Private class to help interact with the Yatzy game object and the GUI
+    private Controller controller = new Controller();
     // Shows the face values of the 5 dice.
     private TextField[] txfValues;
     // Shows the hold status of the 5 dice.
@@ -47,8 +49,10 @@ public class MainAppGUI extends Application {
     private Label lblSumSame, lblBonus, lblSumOther, lblTotal;
     private Label lblRolled;
     private Button btnRoll;
+    private Label lblGameStatus;
     private int rolled = 0;
     private final String ICON_IMG = "dice_icon.png";
+    private boolean txfResultIsFocus = false; //Variable to handle if a Result Textfield has already been selected
 
     // ---------------------------------------------------------------------
 
@@ -75,10 +79,10 @@ public class MainAppGUI extends Application {
         for(int i = 0; i < 5; i++){
             TextField txfDice = new TextField();
             txfDice.setPrefSize(50,50);
+            txfDice.setEditable(false);
             txfDice.setDisable(true);
             txfDice.setFont(Font.font("Verdana", FontWeight.BOLD, 20));
             txfDice.setAlignment(Pos.CENTER);
-            txfDice.setText(String.valueOf(i+1));
             txfValues[i] = txfDice;
             dicePane.add(txfDice, i+1, 1, 1,2);
         }
@@ -86,9 +90,13 @@ public class MainAppGUI extends Application {
         chbHolds = new CheckBox[5];
         for(int i = 0; i < 5; i++){
             CheckBox chbHold = new CheckBox("Hold");
+            chbHold.setDisable(true);
             chbHolds[i] = chbHold;
             dicePane.add(chbHold, i+1, 3, 1, 1);
         }
+
+        lblGameStatus = new Label("Press Roll to start");
+        dicePane.add(lblGameStatus, 1, 4, 2, 1);
 
         btnRoll = new Button("Roll");
         dicePane.add(btnRoll, 4, 4, 1, 1);
@@ -134,10 +142,24 @@ public class MainAppGUI extends Application {
             scorePane.add(lblResult, 0, 1+i, 1, 1);
         }
 
+        //Initializing the 15 different result Textfield in the scorepane and
+        // giving them actions depending on if a txfResult Textfield is selected or not to be able to de-select a Textfield
         txfResults = new TextField[15];
         for (int i = 0; i < 15; i++){
             TextField txfResult = new TextField();
             txfResult.setPrefSize(50, 5);
+            txfResult.setDisable(true);
+            txfResult.setOnMouseClicked(event -> {
+                if(event.getButton() == MouseButton.PRIMARY)
+                    if(txfResultIsFocus){
+                        this.controller.deSelectResultAction(scorePane);
+                    }
+                    else {
+                        this.controller.selectResultAction();
+                    }
+                if(event.getButton() == MouseButton.MIDDLE)
+                    this.controller.easterEggAction();
+            });
             txfResults[i] = txfResult;
             scorePane.add(txfResult, 1, 1+i, 1, 1);
         }
@@ -152,10 +174,15 @@ public class MainAppGUI extends Application {
         txfSumOther = new TextField();
         txfTotal = new TextField();
 
-        txfSumSame.setPrefSize(30, 5);
-        txfBonus.setPrefSize(30, 5);
-        txfSumOther.setPrefSize(30, 5);
-        txfTotal.setPrefSize(30, 5);
+        txfSumSame.setDisable(true);
+        txfBonus.setDisable(true);
+        txfSumOther.setDisable(true);
+        txfTotal.setDisable(true);
+
+        txfSumSame.setPrefSize(40, 5);
+        txfBonus.setPrefSize(40, 5);
+        txfSumOther.setPrefSize(40, 5);
+        txfTotal.setPrefSize(40, 5);
 
         scorePane.add(lblSumSame, 2, 6, 1, 1);
         scorePane.add(txfSumSame, 3, 6, 1, 1);
@@ -168,16 +195,352 @@ public class MainAppGUI extends Application {
 
         scorePane.add(lblTotal, 4, 15, 1, 1);
         scorePane.add(txfTotal, 5, 15, 1, 1);
+
+        btnRoll.setOnAction(event -> {
+            if(!this.controller.getGameStatus())
+                this.controller.startGameAction();
+            else
+                this.controller.rollDiceAction();
+        });
     }
 
-    // -------------------------------------------------------------------------
+    // ---------------------------------------------------------------------
 
-    // TODO: Create a method for btnRoll's action.
-    // Hint: Create small helper methods to be used in the action method.
+    /**
+     * This class controls access to the model in this application. In this
+     * case, the model is a Yatzy game object that controls dice rolls and possible scores for each dice roll.
+     * The controller class itself maintain accumulated points and game rules.
+     */
+    private class Controller{
+        // The Yatzy game object
+        private Yatzy yatzy = new Yatzy();
+        private boolean gameStarted = false; //Variable for btnRoll.setOnAction to check if it needs to call startGameAction() or just rollDiceAction()
+        private boolean gameEnded = false; //Variable to check if startGameAction() can start the game or not.
+        private int resultSelected; //Index variable to use for disabling a txfResult Textfield in txfResults array when calling rollDiceAction()
+        private int throwCount; //Counter for maintaining throws per count | throwCount <= MAX_ROLLS
+        private int roundCount; //Counter for maintaining amount of rounds | roundCount <= MAX_ROUNDS
+        private boolean isSelected; //Check variable to check if a txtResult Textfield has been selected
 
-    // -------------------------------------------------------------------------
+        //Variables to maintain accumulated scores
+        private int sumSame;
+        private int sumBonus;
+        private int sumOther;
+        private int sumTotal;
 
-    // TODO: Create a method for mouse click on one of the text fields in txfResults.
-    // Hint: Create small helper methods to be used in the mouse click method.
+        private final int BONUS_REG = 63; //Required point to get bonus point
+        private final int BONUS = 50; //Bonus point
 
+        private final int MAX_ROLLS = 3; //Maximum rolls per round.
+        private final int MAX_ROUNDS = 15; //Maximum rounds possible before game ends
+
+        private boolean getGameStatus(){
+            return gameStarted;
+        }
+
+        /**
+         * Method to initialize all sum variables and enabling fields
+         */
+        private void startGameAction(){
+            if(!gameEnded) {
+                resetRound();
+
+                gameStarted = true;
+                isSelected = false;
+
+                sumSame = 0;
+                sumBonus = 0;
+                sumOther = 0;
+                sumTotal = 0;
+
+                txfSumSame.setText(String.valueOf(sumSame));
+                txfBonus.setText(String.valueOf(sumBonus));
+                txfSumOther.setText(String.valueOf(sumOther));
+                txfTotal.setText(String.valueOf(sumTotal));
+
+                for (int i = 0; i < 5; i++) {
+                    txfValues[i].setDisable(false);
+                }
+
+                for (int i = 0; i < chbHolds.length; i++) {
+                    chbHolds[i].setDisable(false);
+                }
+                for (int i = 0; i < txfResults.length; i++) {
+                    txfResults[i].setDisable(false);
+                    txfResults[i].setEditable(false);
+                }
+
+                txfSumSame.setDisable(false);
+                txfSumSame.setEditable(false);
+                txfBonus.setDisable(false);
+                txfBonus.setEditable(false);
+                txfSumOther.setDisable(false);
+                txfSumOther.setEditable(false);
+                txfTotal.setDisable(false);
+                txfTotal.setEditable(false);
+
+                btnRoll.setDisable(false);
+
+                lblGameStatus.setText("Game in Progress");
+
+                roundCount = 1;
+
+                //Rolling the dices
+                rollDiceAction();
+            }
+        }
+
+        // -------------------------------------------------------------------------
+
+        // TODO: Create a method for btnRoll's action.
+        // Hint: Create small helper methods to be used in the action method.
+
+        /**
+         * Method to roll dices and maintain visual output for dice and hold textfields updated
+         */
+        private void rollDiceAction(){
+            if (roundCount <= MAX_ROUNDS) {
+                //Checking if a txtResult has not been selected and count of throws is 3 - If so user is alerted and is forced to select a txtResult Textfield
+                if (!isSelected && throwCount >= MAX_ROLLS) {
+                    Alert alert = new Alert(Alert.AlertType.WARNING);
+                    alert.setTitle("Select Result");
+                    alert.setHeaderText("No Result selected");
+                    alert.setContentText("Select a desired and available result");
+                    Stage stage = (Stage) alert.getDialogPane().getScene().getWindow();
+                    stage.getIcons().add(new Image(ICON_IMG));
+                    alert.show();
+                } else if (isSelected) {
+                    resetRound();
+                }
+                //Calculating and setting scores from the 4 sum Textfields
+                calcScores();
+
+                txfSumSame.setText(String.valueOf(sumSame));
+                txfBonus.setText(String.valueOf(sumBonus));
+                txfSumOther.setText(String.valueOf(sumOther));
+                txfTotal.setText(String.valueOf(sumTotal));
+
+                //Disabling the selected txtResult Textfield in txtResults array at 'resultSelect' index if
+                // dices has been thrown and a txfResult Textfield has been selected
+                if (throwCount > 0 && isSelected) {
+                    txfResults[resultSelected].setDisable(true);
+                }
+
+                //Generating array for dices that are being hold
+                boolean[] holds = new boolean[chbHolds.length];
+                for (int i = 0; i < chbHolds.length; i++) {
+                    holds[i] = chbHolds[i].isSelected();
+                }
+
+                yatzy.throwDice(holds);
+
+                throwCount = yatzy.getThrowCount();
+                isSelected = false;
+
+                getDices();
+                getRolls();
+                getResults();
+
+            }
+            else {
+                endGame();
+            }
+        }
+
+        /**
+         * Method to resetting all holds when a result as been selected and increasing roundCount +1
+         */
+        private void resetRound(){
+            for (int i = 0; i < chbHolds.length; i++) {
+                chbHolds[i].setSelected(false);
+            }
+            yatzy.resetThrowCount();
+            roundCount++;
+        }
+
+        /**
+         * Method for maintaining dialog and actions when a game has ended | roundCount <= MAX_ROUNDS
+         */
+        private void endGame(){
+            gameEnded = true;
+            lblGameStatus.setText("Game ended");
+            btnRoll.setDisable(true);
+            txfSumSame.setDisable(true);
+            txfSumOther.setDisable(true);
+            txfBonus.setDisable(true);
+            txfTotal.setDisable(true);
+            for (int i = 0; i < 5; i++) {
+                txfValues[i].setDisable(true);
+            }
+
+            for (int i = 0; i < chbHolds.length; i++) {
+                chbHolds[i].setDisable(true);
+            }
+
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("GAME OVER");
+            alert.setHeaderText("The game has ended \n" + "Total score is: " + txfTotal.getText());
+            alert.setContentText("Would you like to play another game David?");
+            Stage stage = (Stage) alert.getDialogPane().getScene().getWindow();
+            stage.getIcons().add(new Image(ICON_IMG));
+
+            Optional<ButtonType> result = alert.showAndWait();
+            if (result.get() == ButtonType.OK){
+                gameEnded = false;
+                startGameAction();
+            }
+        }
+
+        /**
+         * Method to calculate and set values for the 4 different totals
+         * Calculation is done at dicerolls or 'game end' as this makes it easier to manipulate the live Textfields at runtime
+         */
+        private void calcScores(){
+            if(txfSumSame.getText().trim().isEmpty()){
+                sumSame = 0;
+            }
+            else{
+                sumSame = Integer.valueOf(txfSumSame.getText().trim());
+            }
+
+            if(txfSumOther.getText().trim().isEmpty()){
+                sumOther = 0;
+            }
+            else{
+                sumOther = Integer.valueOf((txfSumOther.getText().trim()));
+            }
+            if(txfBonus.getText().trim().isEmpty()){
+                sumBonus = 0;
+            }
+            else{
+                sumBonus = Integer.valueOf((txfBonus.getText().trim()));
+            }
+            if(txfTotal.getText().trim().isEmpty()){
+                sumTotal = 0;
+            }
+            else{
+                sumTotal = Integer.valueOf((txfTotal.getText().trim()));
+            }
+        }
+
+        /**
+         * Method to show the values of the 5 dices from the Yatzy game object in the 5 txfDice Textfields
+         */
+        private void getDices(){
+            for(int i = 0; i < txfValues.length; i++){
+                txfValues[i].setText(String.valueOf(yatzy.getValues()[i]));
+            }
+        }
+
+        /**
+         * Method to update the lblRolled Label with amount of rolls.
+         */
+        private void getRolls(){
+            if(throwCount <= 3)
+                lblRolled.setText("Rolled: " + throwCount);
+        }
+
+        /**
+         * Method to update the txfResult Textfields with possible results from the yatzy game. Only Textfields in the txfResults array
+         * that is not disabled from the rollDiceAction() method will be set with a value.
+         */
+        private void getResults(){
+            for(int i = 0; i < txfResults.length; i++){
+                if(!txfResults[i].isDisabled()) {
+                    txfResults[i].setText(String.valueOf(yatzy.getPossibleResults()[i]));
+                }
+            }
+        }
+
+        // -------------------------------------------------------------------------
+
+        // TODO: Create a method for mouse click on one of the text fields in txfResults.
+        // Hint: Create small helper methods to be used in the mouse click method.
+
+        /**
+         * Method to set accumulated values in the accumulated Textfields.
+         * Setting the index value of txfResults (resultSelected) for rollDiceAction()
+         * Setting txfResultIsFocus for the txfResult.setOnMouseClicked event.
+         * Setting isSelected for rollDiceAction() to check if a Texfield needs to be disabled.
+         *
+         */
+        private void selectResultAction(){
+            int sameSum = 0;
+            int otherSum = 0;
+            for(int i = 0; i < 15; i++){
+                if(txfResults[i].isFocused() && i < 6){
+                    sameSum = Integer.valueOf(txfResults[i].getText().trim());
+                    resultSelected = i; //Index of the selected Textfield in txfResults array
+                    txfResultIsFocus = true;
+                }
+                else if(txfResults[i].isFocused() && i > 5){
+                    otherSum = Integer.valueOf(txfResults[i].getText().trim());
+                    resultSelected = i; //Index of the selected Textfield in txfResults array
+                    txfResultIsFocus = true;
+                }
+            }
+
+            txfSumSame.setText(String.valueOf(sumSame+sameSum));
+            txfSumOther.setText(String.valueOf(sumOther+otherSum));
+            isSelected = true;
+
+            setBonus();
+            setTotal();
+
+        }
+
+         /**
+         * Method to remove focus from a Result Textfield and reverse any accumulated values from select action
+          * Setting isSelected for rollDiceAction() to false
+          * Taking the current scorePane Gridpane as parameter to be able to force focus the overall scorePane.
+         * @param scorePane
+         */
+        private void deSelectResultAction(GridPane scorePane){
+            for(int i = 0; i < 15; i++){
+                if(txfResults[i].isFocused() && i < 6){
+                    resultSelected = i; //Index of the selected Textfield in txfResults array
+                    txfResultIsFocus = false;
+                }
+                else if(txfResults[i].isFocused() && i > 5){
+                    resultSelected = i; //Index of the selected Textfield in txfResults array
+                    txfResultIsFocus = false;
+                }
+            }
+
+            txfSumSame.setText(String.valueOf(sumSame));
+            txfSumOther.setText(String.valueOf(sumOther));
+
+            isSelected = false;
+            scorePane.requestFocus();
+
+            setBonus();
+            setTotal();
+
+        }
+
+        /**
+         * Method for setting the txfBonus Textfield = 50 if accumulated sum of txfSumSame is min. 63
+         */
+        private void setBonus(){
+            if(Integer.valueOf(txfSumSame.getText().trim()) >= BONUS_REG) {
+                txfBonus.setText(String.valueOf(BONUS));
+            }
+            else
+                txfBonus.setText(String.valueOf(sumBonus));
+        }
+
+        /**
+         * Method for setting txfTotal for all accumulated Textfields
+         */
+        private void setTotal(){
+            int total = Integer.valueOf(txfSumSame.getText().trim()) + Integer.valueOf(txfSumOther.getText().trim()) + Integer.valueOf(txfBonus.getText().trim());
+            txfTotal.setText(String.valueOf(total));
+        }
+
+        private void easterEggAction(){
+            System.out.println("Java is fun, but can be difficult");
+        }
+    }
 }
+
+
+
